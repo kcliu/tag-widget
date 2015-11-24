6cd6ebd98bb6cecba254176809609b657254be45
kcliu <gjliou@cs.nctu.edu.tw>
2015-11-24 22:07:48 +0800

    rewrite to module pattern

--- tag.js
+++ tag.js
@@ -1,41 +1,58 @@
-function fetchJSON(url, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) {
-                    callback(data);
+var tag = function() {
+    var input = document.getElementById("js-input-area");
+    var fetchJSON = function(url, callback) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if (callback) {
+                        callback(data);
+                    }
                 }
             }
         }
+        httpRequest.open("GET", url, true);
+        httpRequest.send("");
     }
-    httpRequest.open("GET", url, true);
-    httpRequest.send("");
-}
 
-function searchCity(input) {
-    console.log("search city:" + input.value);
+    var searchCityHandler= function(input) {
+        console.log("search city:" + input.value);
+        return matchCity(input.value);
+    }
 
-}
+    var createTag = function(tag) {
+        console.log("create tag:" + tag.innerHTML);
 
-function createTag(tag) {
-    console.log("create tag:" + tag.innerHTML);
+    }
 
-}
+    var matchCity = function(subString, dataList) {
+        console.log("matchCity:" + subString);
 
-var input = document.getElementById("js-input-area");
-input.addEventListener("blur", function(event) {
-    var tagbox = document.getElementById("js-tagbox");
-    // http://caniuse.com/#feat=classlist
-    tagbox.classList.add("hide");
-});
+    }
 
-input.addEventListener("focus", function(event) {
-    var tagbox = document.getElementById("js-tagbox");
-    tagbox.classList.remove("hide");
-});
+    input.addEventListener("focus", function(event) {
+        var tagbox = document.getElementById("js-tagbox");
+        tagbox.classList.remove("hide");
+    });
 
-fetchJSON('https://raw.githubusercontent.com/kcliu/tag-widget/master/tz.json', function(data){
+    input.addEventListener("blur", function(event) {
+        var tagbox = document.getElementById("js-tagbox");
+        window.setTimeout(function() {
+            // http://caniuse.com/#feat=classlist
+            tagbox.classList.add("hide");
+        }, 10);
+    });
+
+    return {
+        fetchJSON: fetchJSON,
+        searchCityHandler: searchCityHandler,
+        createTag: createTag,
+        matchCity: matchCity
+    }
+
+}();
+
+tag.fetchJSON('https://raw.githubusercontent.com/kcliu/tag-widget/master/tz.json', function(data){
     console.log(data);
 });
